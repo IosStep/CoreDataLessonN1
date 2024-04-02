aabfab2f3562ae872f77aa33f51b4fdf72f61d93
a.atpv <aldiyar09.02@mail.ru>
2024-04-02 10:01:48 +0500

    initial commit

---
+++ TodoListTask+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  TodoListTask+CoreDataClass.swift
+//  CoreDataLessonN1
+//
+//  Created by Aldiyar Aitpayev on 01.04.2024.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(TodoListTask)
+public class TodoListTask: NSManagedObject {
+
+}
